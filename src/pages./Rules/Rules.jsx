c063155263d3422434c58872a59508ed084fbeda
Nikhil Malviya <94277199+nikk422@users.noreply.github.com>
2022-05-07 15:46:15 +0530

    Added rules and  authentication page
    in  rules page i added some rules of quiz. and in authentication page i added login  and sign up page

---
+++ src/pages./Rules/Rules.jsx
@@ -0,0 +1,23 @@
+import "./rules.css";
+import {Link} from "react-router-dom";
+const Result=()=>{
+    return(
+        <div className="Rules-container">
+            <div className="rules-contain">
+            <img className="rules-img" src="/asset/rules1.png" alt=""/>           
+            <ol className="rules-list">
+                <li>There are total 5 questions in this section.</li>
+                <li>For Each correct answer there will be 2 point.</li>
+                <li>For Each incorrect answer there will be 0 point.</li>
+                <li>You may not change the option once opted</li>
+            </ol>
+            <Link to="/">
+                <button className="start-btn headHover">Start Quiz..</button>
+            </Link>
+            </div>
+ 
+        </div>
+    )
+}
+
+export default Result;
